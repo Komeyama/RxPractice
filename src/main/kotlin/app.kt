d82b49e2fe7bfd11fd763c70b02e0e7f9d376da4
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2021-12-12 01:15:58 +0900

    add simple FromCallable operator

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,17 +1,27 @@
 import operators.FromArrayOperatorExample
+import operators.FromCallableOperatorExample
 import operators.JustOperatorExample
 
 fun main() {
-    initJustExample()
-    initFromArrayOperatorExample()
+    setupJustExample()
+    setupFromArrayOperatorExample()
+    setupFromCallableOperatorExample()
 }
 
-private fun initJustExample() {
+private fun setupJustExample() {
     val justExample = JustOperatorExample()
     justExample.executeJust()
 }
 
-private fun initFromArrayOperatorExample() {
+private fun setupFromArrayOperatorExample() {
     val fromArrayOperatorExample = FromArrayOperatorExample()
     fromArrayOperatorExample.executeFromArray()
+}
+
+private fun setupFromCallableOperatorExample() {
+    val fromCallableOperatorExample = FromCallableOperatorExample()
+    //　Call 'executeFromCallable' twice to confirm time data gets generate every time.
+    fromCallableOperatorExample.executeFromCallable()
+    Thread.sleep(100L)
+    fromCallableOperatorExample.executeFromCallable()
 }
---
+++ src/main/kotlin/operators/FromCallableOperatorExample.kt
@@ -0,0 +1,17 @@
+package operators
+
+import io.reactivex.rxjava3.core.Flowable
+import subscribers.DebugSubscriber
+
+class FromCallableOperatorExample {
+
+    /***
+     * [Result]
+     * current timestamp [ms]
+     * completed!
+     */
+    fun executeFromCallable() {
+        val flowable: Flowable<Long> = Flowable.fromCallable { System.currentTimeMillis() }
+        flowable.subscribe(DebugSubscriber(label = "from_callable"))
+    }
+}
