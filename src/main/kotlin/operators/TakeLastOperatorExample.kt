bebade22f8012c305c9e8518e4e8c4de9c88c212
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2021-12-30 19:31:04 +0900

    add another takeLast example function

--- src/main/kotlin/operators/TakeLastOperatorExample.kt
+++ src/main/kotlin/operators/TakeLastOperatorExample.kt
@@ -12,8 +12,8 @@ class TakeLastOperatorExample {
      * threadName: RxComputationThreadPool-1, data: 4
      * threadName: RxComputationThreadPool-1 completed!
      */
-    fun executeTakeLast() {
-        val label = "take_last"
+    fun executeTakeLastType1() {
+        val label = "take_last_type1"
         val flowable: Flowable<Long> =
             Flowable.interval(300L, TimeUnit.MILLISECONDS)
                 .take(5)
@@ -21,5 +21,22 @@ class TakeLastOperatorExample {
 
         flowable.subscribe(DebugSubscriber(label = label))
         Thread.sleep(2000L)
+    }
+
+    /**
+     * [Result]
+     * threadName: RxComputationThreadPool-2, data: 6
+     * threadName: RxComputationThreadPool-2, data: 7
+     * threadName: RxComputationThreadPool-2 completed!
+     */
+    fun executeTakeLastType2() {
+        val label = "take_last_type2"
+        val flowable: Flowable<Long> =
+            Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .take(8)
+                .takeLast(2, 1000L, TimeUnit.MILLISECONDS)
+
+        flowable.subscribe(DebugSubscriber(label = label))
+        Thread.sleep(3000L)
     }
 }
