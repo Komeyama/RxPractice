63b668e49bd7b6927394929d295807db53280dfa
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2022-01-22 16:08:04 +0900

    update version of gradle, kotlin, etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,41 @@
+//import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+//
+//plugins {
+//    java
+//    kotlin("jvm") version "1.3.41"
+//}
+//
+//group = "com.komeyama.rxpractice"
+//version = "1.0-SNAPSHOT"
+//
+//repositories {
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    implementation(kotlin("stdlib-jdk8"))
+//
+//    // rx
+//    implementation("io.reactivex.rxjava3:rxjava:3.1.1")
+//
+//    testCompile("junit", "junit", "4.12")
+//}
+//
+//configure<JavaPluginConvention> {
+//    sourceCompatibility = JavaVersion.VERSION_1_8
+//}
+//tasks.withType<KotlinCompile> {
+//    kotlinOptions.jvmTarget = "1.8"
+//}
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    java
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.6.0"
+    application
 }
 
-group = "com.komeyama.rxpractice"
+group = "me.yoneyama"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,17 +43,21 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
 
-    // rx
     implementation("io.reactivex.rxjava3:rxjava:3.1.1")
 
-    testCompile("junit", "junit", "4.12")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+tasks.test {
+    useJUnitPlatform()
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
 }
