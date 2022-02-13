bfb6942394a102ad4b1c784e8cb81c7ed936f0f5
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2022-02-13 21:06:39 +0900

    add cancel of flow

--- src/main/kotlin/coroutines/flow/FlowExample.kt
+++ src/main/kotlin/coroutines/flow/FlowExample.kt
@@ -2,18 +2,23 @@ package coroutines.flow
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.lang.Thread.sleep
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class FlowExample {
 
     /**
-     * start time:41:59.140
-     * collect:1, 41:59.375, threadName: main
-     * emit:1, 41:59.375, threadName: main
-     * collect:10, 42:00.384, threadName: main
-     * emit:10, 42:00.384, threadName: main
+     * start time:03:28.832
+     * collect1:1, 03:29.185, threadName: main
+     * emit:1, 03:29.186, threadName: main
+     * collect2:1, 03:29.188, threadName: main
+     * emit:1, 03:29.188, threadName: main
+     * collect1:10, 03:30.188, threadName: main
+     * emit:10, 03:30.189, threadName: main
+     * finish collect1!: 03:31.194
      */
     fun execFlow1() {
         val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("mm:ss.SSS")
@@ -30,10 +35,23 @@ class FlowExample {
         }
 
         runBlocking {
-            flow.collect {
-                val threadName = Thread.currentThread().name
-                println("collect:$it, ${LocalTime.now().format(formatter)}, threadName: $threadName")
+            launch {
+                flow.collect {
+                    val threadName = Thread.currentThread().name
+                    println("collect1:$it, ${LocalTime.now().format(formatter)}, threadName: $threadName")
+                }
+                println("finish collect1!: ${LocalTime.now().format(formatter)}")
             }
+
+            val job = launch {
+                flow.collect {
+                    val threadName = Thread.currentThread().name
+                    println("collect2:$it, ${LocalTime.now().format(formatter)}, threadName: $threadName")
+                }
+                println("finish collect2!: ${LocalTime.now().format(formatter)}")
+            }
+            delay(1000L)
+            job.cancel()
         }
     }
 }
